83002a012e424a129b24124a21df9792a48fe7eb
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-12 01:04:18 +0900

    Add Ex11

---
+++ Day1/Ex11.py
@@ -0,0 +1,11 @@
+words = ['eat','tea','tan','nat']
+anagrams = {}
+
+for word in words:
+    sorted_word = ''.join(sorted(word))# sort the letters of the word and join them back together to form a new string.'' is used to join the letters together without any spaces.      
+    if sorted_word in anagrams:
+        anagrams[sorted_word].append(word)
+    else:
+        anagrams[sorted_word] = [word]# if the sorted_word is not in the anagrams dictionary, create a new key-value pair with the sorted_word as the key and a list containing the original word as the value.
+
+print(list(anagrams.values()))#anagrams.values() take the values, example:[['eat','tea'],['tan','nat']]. list() convert the values of the dictonary into alist.
