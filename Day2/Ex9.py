1ef1322ab87550454fb9df307478d0cf23a152cf
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-13 00:52:54 +0900

    Add Ex9

---
+++ Day2/Ex9.py
@@ -0,0 +1,24 @@
+def singleton(cls): # class image is box, タイ焼きの型
+    instance = None #instance is real example,タイ焼きそのもの
+
+    def wrapper(*args, **kwargs):
+        nonlocal instance # nonlocal is going to use outside of variable, it's mean use "instance" here.
+
+        if instance is None:
+            instance = cls(*args, **kwargs)
+
+        return instance
+
+    return wrapper
+
+#it is differnt singleton of instance and nonlocal of instance
+@singleton
+class MyClass:
+    pass
+
+a = MyClass()
+b = MyClass()
+
+
+
+print(a is b)
