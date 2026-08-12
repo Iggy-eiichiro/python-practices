76be4ae5d12a114186bd95b4adac87fdda259663
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-12 16:09:49 +0900

    Add Ex3

--- Day2/Ex2.py
+++ Day2/Ex2.py
@@ -1,23 +1,23 @@
 def retry(n):
-    def decorator(func):
+    def decorator(func):# decorator is a function that tskes another function as an argument.
         def wrapper(*args, **kwargs):
             for i in range(n):
-                try:
+                try:# when error has possibility to be happened, do this process
                     return func(*args, **kwargs)
-                except Exception:
+                except Exception:# when error already happened, do this process
                     print(f"Retry {i + 1}")
 
             print("Failed")
-            return None
+            return None# many times failed, it's mean there is no success
 
-        return wrapper
+        return wrapper# order to use wrapper
 
-    return decorator
+    return decorator# order to use decorator
 
 
 @retry(3)
 def connect():
-    raise ConnectionError("Connection failed")
+    raise ConnectionError("Connection failed")# on porpose, make it error, because for education
 
 
 connect()
