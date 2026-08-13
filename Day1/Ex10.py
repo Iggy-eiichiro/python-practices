5d72895d21efed5df8b979f5b0709c11d1550fdb
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-13 16:15:43 +0900

    Add Fixed Ex10.py

--- Day1/Ex10.py
+++ Day1/Ex10.py
@@ -2,6 +2,6 @@ nums=[7,11,2]
 target=9
 
 for i in range(len(nums)):# i got the index of the first number
-    for j in range(i+1,len(nums)):# j  got the index of the second number
+    for j in range(i+1,len(nums)):# j  got the index of the second number, if take index(0), range(i+1, len(nums))
         if nums[i]+nums[j]==target:# if the sum of the two numbers equals the target
             print([i,j])
