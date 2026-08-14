74d188d5c633dc1724737203df4b92a73a0485f9
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-14 12:03:28 +0900

    Add Ex8.py

---
+++ Day3/Ex8.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+    def __str__(self):
+             return f"User:{self.name}(ID:{self.user_id})"
+
+
+# def __str__(self):-Here is not working , because outside of calss User
+#     return f"User:{self.name}(ID:{self.user_id})"
+
+user = User(name = "Eiichiro" , user_id = "23A2032")
+
+print(user)
