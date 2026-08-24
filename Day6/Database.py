05c096d2b3acfb13c51706224371aa144b4f14e0
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-24 18:49:59 +0900

    Add Ex3.py

--- Day6/Database.py
+++ Day6/Database.py
@@ -1,10 +1,14 @@
-from sqlalchemy import create_engine# Building an Engine to Connect to a Database
-from sqlalchemy.orm import declarative_base, sessionmaker # This is used to define Python classes as database tables. 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://postgres.ahtvumqsnxlvspnasbpn:Pass%40bcd122333@aws-0-ap-northeast-1.pooler.supabase.com:5432/pagila"
 
-engine = create_engine(DATABASE_URL)#Please use the database connection information stored in DATABASE_URL to connect.
+engine = create_engine(DATABASE_URL)
 
-Base = declarative_base()# Now I'm going to create a database table using a Python class.
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-SessionLocal = sessionmaker(bind=engine)#session,An interface for exchanging data between Python and a database
+Base = declarative_base()
