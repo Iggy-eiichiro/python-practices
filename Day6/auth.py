341e0aa136d2e6ed1487fbe9f2930c7a68107bb9
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-25 21:34:09 +0900

    Add Ex10

--- Day6/auth.py
+++ Day6/auth.py
@@ -1,21 +1,58 @@
 import jwt
 
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 
-SECRET_KEY = "my-secret-key"# for learning. basically do not write like that.
-#import os SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"# when i make jwt, use this ALGORITHM
+
+SECRET_KEY = "my-secret-key"
+ALGORITHM = "HS256"
+
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="login"
+)
 
 
 def create_access_token(user_id: int):
 
     data = {
-        "sub": str(user_id)# sub = subject
+        "sub": str(user_id)#subject
     }
 
-    token = jwt.encode( # jwt make jwt encode, and put into token
+    token = jwt.encode(
         data,
         SECRET_KEY,
         algorithm=ALGORITHM
     )
 
     return token
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme)
+):
+
+    try:
+
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        user_id = payload.get("sub")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=401,
+                detail="Invalid token"
+            )
+
+        return int(user_id)
+
+    except jwt.InvalidTokenError:
+
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid token"
+        )
