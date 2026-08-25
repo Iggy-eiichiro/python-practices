78ed5736f8b55e036244ed5a17f8e40104d5a1b6
Eiichiro Kawada <kawada.eiichiro@rikai.co.jp>
2026-08-25 21:34:09 +0900

    Add Ex10

--- Day6/main.py
+++ Day6/main.py
@@ -1,44 +1,53 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from Database import get_db
 from models import User
-from schemas import UserLogin
-from auth import create_access_token
+from auth import create_access_token, get_current_user
 
 
 app = FastAPI()
 
 
+# 09 JWT Authentication
 @app.post("/login")
 def login(
-    user_data: UserLogin,
+    form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
 
-    # Find the user
     user = db.query(User).filter(
-        User.name == user_data.name
+        User.name == form_data.username
     ).first()
 
-    # User does not exist
     if user is None:
         raise HTTPException(
             status_code=401,
             detail="Incorrect name or password"
         )
 
-    # Check the password
-    if user.password != user_data.password:
+    if user.password != form_data.password:
         raise HTTPException(
             status_code=401,
             detail="Incorrect name or password"
         )
 
-    # Create JWT
     access_token = create_access_token(user.id)
 
     return {
         "access_token": access_token,
         "token_type": "bearer"
+    }
+
+
+# 10 Protected APIs
+@app.get("/profile")
+def get_profile(
+    user_id: int = Depends(get_current_user)
+):
+
+    return {
+        "message": "You are authenticated",
+        "user_id": user_id
     }
